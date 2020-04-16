47eee17137ce6ed2e8929c8a1863ab310f10df12
Ville Kautto <ville.kautto@hotmail.fi>
2020-04-16 17:39:02 +0300

    add header, footer and adjust blog width

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>This is a footer</p>
+            </div>
+        );
+    }
+    
+}
+
+export default Footer;
