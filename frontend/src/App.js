07fc1c27418f0fd43c504baaa4cac955b8a1e359
Ville Kautto <ville.kautto@hotmail.fi>
2020-05-06 20:16:36 +0300

    add search bar for blog browsing
    Added a search bar for better browsing experience

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,43 @@ import NavBar from './components/NavBar/NavBar'
 import Header from './components/Header/Header'
 import Blog from './components/Blog/Blog'
 import Footer from './components/Footer/Footer'
+import TextField from '@material-ui/core/TextField';
 
+/**
+ * Handles the search queries, returns all blogs that contain matching text in the title or body.
+ * Returns all if no keywords are specified
+ */
+function searchFilter(keyword) {
+  return function(blog) {
+    return blog.title.toLowerCase().includes(keyword.toLowerCase()) ||
+      blog.body.toLowerCase().includes(keyword.toLowerCase()) || !keyword;
+  }
+}
+
+/**
+ * Contains all the components displayed on the website
+ */
 class App extends React.Component {
-  state = {
-    blogs: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogs: [],
+      keyword: ''
+    }
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+  
+  /**
+   * Changes keyword's state based on the search bar's content
+   * @param {*} event handles search bar's onChange events
+   */
+  searchHandler(event) {
+    this.setState( {keyword: event.target.value})
   }
 
+  /**
+   * Fetches data from the database upon loading
+   */
   componentDidMount() {
     fetch('http://localhost:8080/blogs')
     .then(res => res.json())
@@ -20,13 +51,29 @@ class App extends React.Component {
     .catch(console.log)
   }
 
+  /**
+   * Handles the site layout and creates blog objects to the site
+   */
   render() {
-    const blogs = this.state.blogs.map(blog => <Blog key={blog.id} id={blog.id} title={blog.title} body={blog.body} datetime={blog.datetime} comments={blog.comments} />)
-    console.log(blogs)
+    // Mapping blogs and all of their values
+    const blogs = this.state.blogs.filter(searchFilter(this.state.keyword))
+      .map(blog => <Blog key={blog.id} id={blog.id}title={blog.title} body={blog.body}
+      datetime={blog.datetime} comments={blog.comments} />)
+    console.log(blogs);
+
+    /**
+     * Site's object structure
+     */
     return (
       <div className="app">
         <NavBar/>
         <Header/>
+        <TextField
+          className="searchbar"
+          variant="outlined"
+          type="text"
+          label="Search for blogs"
+          onChange={this.searchHandler} />
         <div className="blogs">
           {blogs}
         </div>
